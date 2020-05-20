4035ac47f25443031f2386ca129e2f15314ee2ed
gandh99 <gandh99@hotmail.com>
2020-05-20 15:36:44 +0800

    Messages created will be sorted by timestamp

--- client/src/redux/reducers/createNoteReducer.tsx
+++ client/src/redux/reducers/createNoteReducer.tsx
@@ -1,5 +1,6 @@
 import { createNote } from '../actionTypes'
 import { IMessage } from '../../utils/note'
+import { sortByTimestamp } from '../../utils/createNote'
 
 interface IInitialState {
     newMessage?: IMessage,
@@ -12,7 +13,7 @@ const initialState = {
     timestamp: '',
 
     // For recording the messages written by the user
-    newMessage: { uuid: '', timestamp: '', text: ''},
+    newMessage: { uuid: '', timestamp: '', text: '' },
     allMessages: []
 } as IInitialState      // Necessary format to include properties not defined in IInitialState
 
@@ -36,11 +37,11 @@ export default function (state = initialState, action: any) {
                 timestamp: ''
             }
         case createNote.CREATE_MESSAGE:
-            console.log(action.payload)
             return {
                 ...state,
                 newMessage: action.payload,
                 allMessages: [...state.allMessages!, action.payload]
+                    .sort((m1: IMessage, m2: IMessage) => sortByTimestamp(m1.timestamp, m2.timestamp))
             }
         default:
             return state
--- client/src/utils/createNote/index.tsx
+++ client/src/utils/createNote/index.tsx
@@ -18,3 +18,18 @@ export const formatTimestamp = (timeElapsedInSeconds: number): string => {
 
     return times.join(':')
 }
+
+export const getTimeElapsedInSeconds = (timestamp: string): number => {
+    const timeUnits: string[] = timestamp.split(':')
+    return Number(timeUnits[0]) * 3600 + Number(timeUnits[1]) * 60 + Number(timestamp[0])
+}
+
+export const sortByTimestamp = (timestamp1: string, timestamp2: string): number => {
+    if (!timestamp1.match(/\d+:\d{2}:\d{2}/) || !timestamp2.match(/\d+:\d{2}:\d{2}/)) {
+        throw new Error('Improper timestamp format. Timestamp format should be H:MM:SS')
+    }
+
+    const timeElapsed1 = getTimeElapsedInSeconds(timestamp1)
+    const timeElapsed2 = getTimeElapsedInSeconds(timestamp2)
+    return timeElapsed1 - timeElapsed2
+}
