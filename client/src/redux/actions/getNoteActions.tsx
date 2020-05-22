d6112795012f5667535064459b15778af1b80aa4
gandh99 <gandh99@hotmail.com>
2020-05-22 16:14:45 +0800

    When viewing all notes, the notes are sorted by latest first

--- client/src/redux/actions/getNoteActions.tsx
+++ client/src/redux/actions/getNoteActions.tsx
@@ -1,14 +1,22 @@
 import { getNote } from '../actionTypes'
 import axios from '../../config/axiosConfig'
 import { returnErrors } from './errorActions'
+import { INote } from '../../interfaces/notes'
 
 export const getAllNotesAction = (success: Function, error: Function) => (dispatch: any) => {
     axios
         .get('/notes/get-all-notes')
         .then(res => {
+            let allNotes: INote[] = res.data.data
+
+            // Sort by latest first
+            allNotes.sort((note1: INote, note2: INote) => {
+                return (note1.updatedAt! > note2.updatedAt!) ? -1 : 1
+            })
+
             dispatch({
                 type: getNote.GET_ALL_NOTES_SUCCESS,
-                payload: res.data.data
+                payload: allNotes
             })
             success()
         })
