6ffbda9e74f46233b699566d5cba61fe2b5fdbd0
gandh99 <gandh99@hotmail.com>
2020-05-23 10:18:20 +0800

    All categories now show up in the CategoryDialog. Adding a new category also automatically adds it to the list.

--- client/src/allNotes/header/CategoryDialog.tsx
+++ client/src/allNotes/header/CategoryDialog.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Dialog, DialogTitle, DialogContent, InputBase, Divider } from '@material-ui/core'
 import CategoryItem from './CategoryItem'
 import CategoryInput from './CategoryInput'
@@ -8,6 +8,7 @@ import CategoryInput from './CategoryInput'
 export default function CategoryButton(props: { open: boolean, setOpen: Function }) {
     const classes = useStyles()
     const dispatch = useDispatch()
+    const allCategories = useSelector((state: any) => state.category.allCategories)
 
     const handleClose = () => {
         props.setOpen(false)
@@ -23,6 +24,9 @@ export default function CategoryButton(props: { open: boolean, setOpen: Function
             <DialogTitle id="form-dialog-title">View by Category</DialogTitle>
             <DialogContent className={classes.content}>
                 <CategoryItem text={'All'} deletable={false} />
+                {allCategories.map((category: any, index: number) =>
+                    <CategoryItem key={index} text={category.name} deletable={true} />
+                )}
                 <Divider variant='middle' />
                 <div className={classes.addCategory}>
                     <CategoryInput />
