9eae3e6d2fc397d74f8b9cdbaf904b3e02de8a6f
gandh99 <gandh99@hotmail.com>
2020-05-23 18:05:26 +0800

    A note will have its category displayed in a chip

--- client/src/allNotes/body/NoteCard.tsx
+++ client/src/allNotes/body/NoteCard.tsx
@@ -2,21 +2,30 @@ import React, { useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import { useDispatch } from 'react-redux'
 import { INote } from '../../interfaces/notes'
-import { Card, Typography, CardContent, Divider, Grid } from '@material-ui/core'
+import { Card, Typography, CardContent, Divider, Grid, Chip } from '@material-ui/core'
 import { formatScribble, formatTimestamp } from '../../utils/allNotes'
 import Timestamp from './Timestamp'
 
 export default function NoteCard(props: { note: INote }) {
     const classes = useStyles()
-    const { title, videoUrl, updatedAt, allScribbles } = props.note
+    const { title, videoUrl, category, updatedAt, allScribbles } = props.note
 
     return (
         <Grid item xs={12} sm={12} md={6} lg={4}>
             <Card className={classes.root}>
                 <CardContent>
-                    <Typography className={classes.title} variant="h5" component="h2">
-                        {title}
-                    </Typography>
+                    <div className={classes.header}>
+                        <Typography className={classes.title} variant="h5" component="h2">
+                            {title}
+                        </Typography>
+                        {category?.categoryId &&
+                            <Chip
+                                className={classes.chip}
+                                label={category.name}
+                                size='small'
+                            />
+                        }
+                    </div>
                     <Divider className={classes.divider} />
                     <div className={classes.scribblesArea}>
                         {allScribbles[0] && <p>{formatScribble(allScribbles[0].text)}</p>}
@@ -37,19 +46,39 @@ const useStyles = makeStyles((theme) => ({
         margin: '1rem',
         cursor: 'pointer'
     },
+
+    // Header
+    header: {
+        display: 'flex',
+        flexDirection: 'row'
+    },
     title: {
         fontSize: 16,
         fontWeight: 'bold',
         textAlign: 'left'
     },
+    chip: {
+        marginLeft: 'auto',
+        cursor: 'pointer',
+        fontWeight: 'bold',
+        fontSize: 14,
+        backgroundColor: theme.palette.secondary.main,
+        '&:hover': {
+            backgroundColor: theme.palette.secondary.dark
+        }
+    },
     divider: {
         margin: '0.5rem 0'
     },
+
+    // Body
     scribblesArea: {
         textAlign: 'left',
         color: theme.palette.grey[600],
         fontSize: 14
     },
+
+    // Footer
     footer: {
         display: 'flex',
         flexDirection: 'row',
