965954fa22c050aaa41e365801f647490b909d98
gandh99 <gandh99@hotmail.com>
2020-05-24 15:20:15 +0800

    Minor changes

--- client/src/createNote/body/NoteBody.tsx
+++ client/src/createNote/body/NoteBody.tsx
@@ -3,7 +3,6 @@ import { Card } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import VideoPlayer from '../../reusableComponents/videoPlayer/VideoPlayer'
 import ScribbleDisplayArea from './ScribbleDisplayArea'
-import { useDispatch } from 'react-redux'
 
 export default function NoteBody() {
     const classes = useStyles()
