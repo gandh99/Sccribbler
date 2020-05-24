c54dbfa89e5e106800f42ee9c7ff39a4a956c34c
gandh99 <gandh99@hotmail.com>
2020-05-24 16:41:40 +0800

    The header is now clickable

--- client/src/reusableComponents/noteCard/Header.tsx
+++ client/src/reusableComponents/noteCard/Header.tsx
@@ -3,16 +3,23 @@ import { makeStyles } from '@material-ui/core/styles'
 import { Typography } from '@material-ui/core'
 import MenuDisplay from './MenuDisplay'
 import { INote } from '../../interfaces/notes'
+import { Link } from 'react-router-dom'
 
-export default function Header(props: { note: INote }) {
+export default function Header(props: { note: INote, loadNoteData: Function }) {
     const classes = useStyles()
-    const { note } = props
+    const { note, loadNoteData } = props
 
     return (
         <div className={classes.root}>
-            <Typography className={classes.title} variant="h5" component="h2">
-                {note.title}
-            </Typography>
+            <Link
+                onClick={() => loadNoteData(note.title, note.videoUrl, note.category, note.allScribbles)}
+                to={'/edit-note'}
+                style={{ textDecoration: 'none' }}
+                className={classes.link}>
+                <Typography className={classes.title} variant="h5" component="h2">
+                    {note.title}
+                </Typography>
+            </Link>
             <MenuDisplay note={note} />
         </div>
     )
@@ -21,11 +28,15 @@ export default function Header(props: { note: INote }) {
 const useStyles = makeStyles((theme) => ({
     root: {
         display: 'flex',
-        flexDirection: 'row'
+        flexDirection: 'row',
+    },
+    link: {
+        color: theme.palette.grey[700],
+        width: '100%'
     },
     title: {
         fontSize: 16,
         fontWeight: 'bold',
-        textAlign: 'left'
+        textAlign: 'left',
     },
 }))
--- client/src/reusableComponents/noteCard/NoteCard.tsx
+++ client/src/reusableComponents/noteCard/NoteCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import { INote } from '../../interfaces/notes'
+import { INote, IScribble } from '../../interfaces/notes'
 import { Card, CardContent, Divider, Grid } from '@material-ui/core'
 import Header from './Header'
 import Body from './Body'
@@ -9,13 +9,19 @@ import { Link } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { saveTitleAction, saveVideoUrlAction, saveAllScribblesAction } from '../../redux/actions/saveNoteActions'
 import { setActiveCategoryAction } from '../../redux/actions/categoryActions'
+import { ICategory } from '../../interfaces/category'
 
 export default function NoteCard(props: { note: INote }) {
     const classes = useStyles()
     const dispatch = useDispatch()
     const { title, videoUrl, category, updatedAt, allScribbles } = props.note
 
-    const loadNoteData = (): void => {
+    const loadNoteData = (
+        title: string,
+        videoUrl: string,
+        category: ICategory | undefined,
+        allScribbles: IScribble[]
+    ): void => {
         dispatch(saveTitleAction(title))
         dispatch(saveVideoUrlAction(videoUrl))
         dispatch(saveAllScribblesAction(allScribbles))
@@ -29,9 +35,13 @@ export default function NoteCard(props: { note: INote }) {
         <Grid item xs={12} sm={12} md={6} lg={4}>
             <Card className={classes.root}>
                 <CardContent className={classes.cardContent}>
-                    <Header note={props.note} />
+                    <Header note={props.note} loadNoteData={loadNoteData} />
                     <Divider className={classes.divider} />
-                    <Link onClick={loadNoteData} to={'/edit-note'} style={{ textDecoration: 'none' }} className={classes.link}>
+                    <Link
+                        onClick={() => loadNoteData(title, videoUrl, category, allScribbles)}
+                        to={'/edit-note'}
+                        style={{ textDecoration: 'none' }}
+                        className={classes.link}>
                         <Body allScribbles={allScribbles} />
                         <Footer
                             timestamp={updatedAt}
