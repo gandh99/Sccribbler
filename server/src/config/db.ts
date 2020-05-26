ccde6c0661fe5d183465d5788b753e1d04c8f0df
gandh99 <gandh99@hotmail.com>
2020-05-26 21:35:29 +0800

    Updated db config

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,6 +1,17 @@
 const { Client } = require('pg');
 
-const client = new Client()
+let client
+if (process.env.NODE_ENV === 'development') {
+    client = new Client()
+} else if (process.env.NODE_ENV === 'production') {
+    client = new Client({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    })
+}
+
 client
     .connect()
     .then(() => console.log(`Connected to PostgreSQL database on port ${process.env.PGPORT}...`))
